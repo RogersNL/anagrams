90f6e8a3614b0eb073c60751a7fe8bbd2859368c
Nick Rogers and Swati Sahay <nick@>
2018-06-27 10:48:03 -0700

    Create Initial Project Folder.

---
+++ Anagram.Tests/ModelsTests/AnagramTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Anagram;
+
+namespace Anagram.Tests
+{
+  [TestClass]
+  public class AnagramTest
+  {
+    [TestMethod]
+    public void ExampleTest_True()
+    {
+      // eventually your testing code will go here
+    }
+  }
+}
---
+++ Anagram/Models/Anagram.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace Anagram
+{
+public class Anagram
+  {
+  public bool ExampleTest(int year)
+    {
+      // logic will go here
+      return false;
+    }
+  }
+}
